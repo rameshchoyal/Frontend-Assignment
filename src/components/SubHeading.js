6d4d414592d4e6f1b437dca8e77bfcd9767437e3
Ramesh Choyal <krsna@Krsnas-MacBook-Air.local>
2024-07-27 15:38:40 +0530

    Image tag is updated

--- src/components/SubHeading.js
+++ src/components/SubHeading.js
@@ -1,14 +1,17 @@
 // src/components/SubHeading.js
 
+import Image from "next/image";
+
 const SubHeading = () => {
   return (
     <div className="flex items-center justify-between p-4  shadow-sm ">
       <div className="flex items-center">
         <button className="mr-4 text-xl">
-          <img
+          <Image
             src="https://cdn-icons-png.flaticon.com/128/507/507257.png"
             alt="Back Arrow"
-            className="w-6 h-6"
+            width={24}
+            height={24}
           />
         </button>
         <h1 className="text-3xl font-serif  ">Test 3_staging</h1>
